207a765bb9ab0473a9c407c11414a15dca10e214
Wallace, Ann <Ann.Wallace@nike.com>
2017-01-26 17:23:36 -0800

    added vault path and get sekret

--- CerberusMiniClient.py
+++ CerberusMiniClient.py
@@ -6,7 +6,7 @@ from pprint import pprint
 
 class CerberusMiniClient(object):
     """THis is a Cerberus Mini Client Class that does Cerberus stuff"""
-    HEADERS = {'Content-Type' : 'application/json'}
+    HEADERS = {"Content-Type": "application/json"}
 
     def __init__(self, username, password):
         self.cerberus_url = 'https://prod.cerberus.nikecloud.com/'
@@ -23,7 +23,7 @@ class CerberusMiniClient(object):
         else:
             token_resp = auth_resp
         token = token_resp['data']['client_token']['client_token']
-        self.token = str(token)
+        self.token = token
 
     def get_token(self):
         """Returns a client token from Cerberus"""
@@ -35,8 +35,8 @@ class CerberusMiniClient(object):
         if auth_resp.status_code != 200:
            print("ERROR: " + auth_resp.json()['errors'][0]['message'])
            sys.exit(2)
-        auth_resp = json.loads(auth_resp.text)
-        return auth_resp
+        auth_resp_json = json.loads(auth_resp.text)
+        return auth_resp_json
 
     def get_mfa(self, auth_resp):
         """Gets MFA code from user and returns response which includes the client token"""
@@ -51,36 +51,54 @@ class CerberusMiniClient(object):
         if mfa_resp.status_code != 200:
             print("ERROR: " + auth_resp['errors'][0]['message'])
             sys.exit(2)
-        mfa_resp = json.loads(mfa_resp.text)
-        return mfa_resp
+        mfa_resp_json = json.loads(mfa_resp.text)
+        return mfa_resp_json
 
     def get_sdb_path(self,sdb):
-        """Returns the paths for a SDB"""
+        """Returns the path for a SDB"""
         id = self.get_sdb_id(sdb)
         sdb_resp = requests.get(self.cerberus_url + '/v1/safe-deposit-box/' + id + '/',
                                 headers={'Content-Type' : 'application/json', 'X-Vault-Token': self.token})
         if sdb_resp.status_code != 200:
            print("ERROR: " + sdb_resp.json()['errors'][0]['message'])
            sys.exit(2)
-        sdb_resp = json.loads(sdb_resp.text)
-        print("SBD", sdb_resp)
+        sdb_resp_json = json.loads(sdb_resp.text)
+        path = sdb_resp_json['path']
         return path
 
-    def get_sdb_list(self,path):
-        """returns list of XXXXX"""
-        XXX_resp = requests.get(self.cerberus_url + '/v1/secret/' + path + '/?list=true',
-                                headers={'Content-Type' : 'application/json', 'X-Vault-Token': self.token})
+    def get_sdb_keys(self,path):
+        """Returns the keys for a SDB, which are need for the full vault path"""
+        list_resp = requests.get(self.cerberus_url + '/v1/secret/' + path + '/?list=true',
+                                headers= {'Content-Type' : 'application/json', 'X-Vault-Token': self.token})
+        if list_resp.status_code != 200:
+           print("ERROR: " + list_resp.json()['errors'][0]['message'])
+           sys.exit(2)
+        list_resp_json = json.loads(list_resp.text)
+        return list_resp_json['data']['keys']
 
     def get_sdb_id(self,sdb):
         """ Return the ID for the given safety deposit box"""
         sdb_resp = requests.get(self.cerberus_url + '/v1/safe-deposit-box',
-                                headers={'Content-Type' : 'application/json', 'X-Vault-Token': self.token})
+                                headers= {'Content-Type' : 'application/json', 'X-Vault-Token': self.token})
         if sdb_resp.status_code != 200:
            print("ERROR: " + sdb_resp.json()['errors'][0]['message'])
            sys.exit(2)
-        sdb_resp = json.loads(sdb_resp.text)
-        for r in sdb_resp:
+        sdb_resp_json = json.loads(sdb_resp.text)
+        for r in sdb_resp_json:
             if r['name'] == sdb:
                 return str(r['id'])
         print("ERROR: " + sdb + " not found")
         sys.exit(2)
+
+    def get_secret(self,vault_path,key):
+        secret_resp = requests.get(cerberus_url + '/v1/secret/' + vault_path,
+                                  headers={'Content-Type' : 'application/json', 'X-Vault-Token': token})
+        if secret_resp.status_code != 200:
+            print("ERROR: " + secret_resp.json()['errors'][0]['message'])
+            sys.exit(2)
+        secret_resp_json = json.loads(secret_resp.text)
+        if key in secret_resp_json['data']:
+            return secret_resp_json['data'][key])
+        else:
+            print("ERROR: key " + key + " not found")
+            sys.exit(2)
