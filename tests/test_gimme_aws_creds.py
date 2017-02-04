e38fa5c78be32fef357c930fbb9b0f7d5548fdbe
Wallace, Ann <Ann.Wallace@nike.com>
2017-02-03 16:50:55 -0800

    added login test

--- tests/test_gimme_aws_creds.py
+++ tests/test_gimme_aws_creds.py
@@ -1,10 +1,11 @@
-# Standard library imports...
-from unittest.mock import Mock, patch
+# Stuff for tests...
+from unittest.mock import Mock, patch, MagicMock
+from nose.tools import assert_equals, assert_dict_equal
 
-# Third-party imports...
-from nose.tools import assert_equals
+# other stuff
 import argparse
 import getpass
+import json
 
 # Local imports...
 from gimme_aws_creds import GimmeAWSCreds
@@ -15,6 +16,8 @@ class TestGimmeAWSCreds(object):
     def setup_class(self):
         self.gac = GimmeAWSCreds()
         self.gac.okta_api_key = 'XXXXXX'
+        self.gac.idp_entry_url = 'https://example.okta.com'
+        self.maxDiff = None
 
     def test_get_headers(self):
         header = self.gac.get_headers()
@@ -28,6 +31,15 @@ class TestGimmeAWSCreds(object):
         assert_equals(self.gac.username, 'ann')
 
     @patch('getpass.getpass', return_value='1234qwert')
-    def test_get_user_creds(self,mock_input):
+    def test_get_password(self,mock_input):
         self.gac.get_user_creds()
         assert_equals(self.gac.password, '1234qwert')
+
+    @patch('requests.post')
+    def test_get_login_response(self,mock_post):
+        login = '{"expiresAt":"2017-02-04T00:26:24.000Z", "status":"SUCCESS", "sessionToken":"20111ZTiraxruM-0S_nbP3KoaY6nzSVaDYkBoaA3cQh7RgG9lMqPiVk", "_embedded":{"user":{"id":"00u937qp9ym9stuhL0h7", "profile":{"login":"Ann.Wallace@nike.com", "firstName":"Ann","lastName":"Wallace", "locale":"en","timeZone":"America/Los_Angeles"}}}}'
+        mock_post.return_value = Mock()
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.text = login
+        response = self.gac.get_login_response()
+        assert_dict_equal(response, json.loads(login))
