3e92be5dad45388b658b0152203cf761a1f099e4
Pierce, Eric <Eric.Pierce@nike.com>
2017-06-14 14:50:01 -0700

    Use gimme_creds_server instead of the Okta APIs
    Switching to the gimme_creds_server removes the need for an Okta API token in the gimme_creds client

--- gimme_aws_creds/okta.py
+++ gimme_aws_creds/okta.py
@@ -42,16 +42,18 @@ class OktaClient(object):
         self._login_saml_form_action = None
         self._login_saml_relay_state = None
 
-        # self._get_aws_info()
+        self.aws_access = None
+
+        self.req_session = requests.Session()
 
     def set_username(self, username):
         self._username = username
 
-    def login(self, embed_link):
-        """ set the user credentials"""
+    def login(self, embed_link, gimme_creds_server_url):
+        """ Login to Okta and request data from the gimme-creds-server"""
         self._server_embed_link = embed_link
         self._start_login_flow()
-        self._get_aws_account_info()
+        self._get_aws_account_info(gimme_creds_server_url)
 
     def _get_headers(self):
         """sets the default headers"""
@@ -62,11 +64,11 @@ class OktaClient(object):
 
     def _start_login_flow(self):
         """ gets the starts the authentication flow with Okta"""
-        response = requests.get(self._server_embed_link , allow_redirects=False)
+        response = self.req_session.get(self._server_embed_link , allow_redirects=False)
         url_parse_results = urlparse(response.headers['Location'])
         stateToken =  parse_qs(url_parse_results.query)['stateToken'][0]
 
-        response = requests.post(
+        response = self.req_session.post(
             self._okta_org_url + '/authn',
             json={'stateToken': stateToken},
             headers=self._get_headers()
@@ -76,7 +78,7 @@ class OktaClient(object):
     def _login_username_password(self, stateToken, url):
         """ login to Okta with a username and password"""
         creds = self._get_username_password_creds()
-        response = requests.post(
+        response = self.req_session.post(
             url,
             json={'stateToken': stateToken, 'username': creds['username'], 'password': creds['password']},
             headers=self._get_headers()
@@ -85,7 +87,7 @@ class OktaClient(object):
 
     def _login_get_saml_response(self, url):
         """ return the base64 SAML value object from the SAML Response"""
-        response = requests.get(url, verify=self._verify_ssl_certs)
+        response = self.req_session.get(url, verify=self._verify_ssl_certs)
 
         saml_soup = BeautifulSoup(response.text, "html.parser")
         self._login_saml_form_action = saml_soup.find('form', id='appForm').get('action')
@@ -115,167 +117,88 @@ class OktaClient(object):
         else:
             raise RuntimeError('Unknown login status: ' + status)
 
-    def _get_aws_account_info(self):
-        s = requests.Session()
-        s.post(
+    def _get_aws_account_info(self, gimme_creds_server_url):
+        """ Submit the SAMLResponse and retreive the user's AWS accounts from the gimme_creds_server"""
+        self.req_session.post(
             self._login_saml_form_action,
             data = {'SAMLResponse':self._login_saml_response, 'RelayState':self._login_saml_relay_state},
             verify = self._verify_ssl_certs
         )
 
-        response = s.get('https://localhost:8443/api/v1/accounts', verify = self._verify_ssl_certs)
+        api_url = gimme_creds_server_url + '/api/v1/accounts'
+        response = self.req_session.get(api_url, verify = self._verify_ssl_certs)
+        self.aws_access = response.json()
 
-        print(self._login_saml_form_action)
+        # Throw an error if we didn't get any accounts back
+        if self.aws_access == []:
+            print("No AWS accounts found")
+            exit()
 
-        print(response.text)
+    def choose_app(self):
+        """ gets a list of available apps and
+        ask the user to select the app they want
+        to assume a roles for and returns the selection
+        """
+        #app_resp = self.get_app_links()
+        print("Pick an app:")
+        # print out the apps and let the user select
+        for i, app in enumerate(self.aws_access):
+            print('[', i, ']', app["name"])
 
-        exit()
+        selection = input("Selection: ")
 
+        # make sure the choice is valid
+        if int(selection) > len(self.aws_access):
+            print("You selected an invalid selection")
+            sys.exit(1)
 
-    # def get_app_links(self):
-    #     """ return appLinks obejct for the user """
-    #     headers = self._get_headers()
-    #
-    #     response = requests.get(
-    #         self._okta_org_url + '/users/' + self._user_id + '/appLinks',
-    #         headers=headers,
-    #         verify=True
-    #     )
-    #     app_resp = json.loads(response.text)
-    #
-    #     # create a list from appName = amazon_aws
-    #     apps = []
-    #     for app in app_resp:
-    #         if app['appName'] == 'amazon_aws':
-    #             apps.append(app)
-    #
-    #     if 'errorCode' in app_resp:
-    #         print("APP LINK ERROR: " + app_resp['errorSummary'], "Error Code ", app_resp['errorCode'])
-    #         sys.exit(2)
-    #
-    #     return apps
-    #
-    # def get_app(self):
-    #     """ gets a list of available apps and
-    #     ask the user to select the app they want
-    #     to assume a roles for and returns the selection
-    #     """
-    #     app_resp = self.get_app_links()
-    #     print("Pick an app:")
-    #     # print out the apps and let the user select
-    #     for i, app in enumerate(app_resp):
-    #         print('[', i, ']', app["label"])
-    #
-    #     selection = input("Selection: ")
-    #
-    #     # make sure the choice is valid
-    #     if int(selection) > len(app_resp):
-    #         print("You selected an invalid selection")
-    #         sys.exit(1)
-    #
-    #     # delete
-    #     return app_resp[int(selection)]["label"]
-    #
-    # def get_role(self, aws_appname):
-    #     """ gets a list of available roles and
-    #     ask the user to select the role they want
-    #     to assume and returns the selection
-    #     """
-    #     # get available roles for the AWS app
-    #     headers = self._get_headers()
-    #     response = requests.get(
-    #         self._okta_org_url + '/apps/?filter=user.id+eq+\"' +
-    #         self._user_id + '\"&expand=user/' + self._user_id + '&limit=200',
-    #         headers=headers,
-    #         verify=True
-    #     )
-    #     role_resp = json.loads(response.text)
-    #
-    #     # Check if this is a valid response
-    #     if 'errorCode' in role_resp:
-    #         print("ERROR: " + role_resp['errorSummary'], "Error Code ", role_resp['errorCode'])
-    #         sys.exit(2)
-    #
-    #     # print out roles for the app and let the uesr select
-    #     for app in role_resp:
-    #         if app['label'] == aws_appname:
-    #             print("Pick a role:")
-    #             roles = app['_embedded']['user']['profile']['samlRoles']
-    #
-    #             for i, role in enumerate(roles):
-    #                 print('[', i, ']:', role)
-    #             selection = input("Selection: ")
-    #
-    #             # make sure the choice is valid
-    #             if int(selection) > len(roles):
-    #                 print("You selected an invalid selection")
-    #                 sys.exit(1)
-    #
-    #             return roles[int(selection)]
-    #
-    # def get_app_url(self, aws_appname):
-    #     """ return the app link json for select aws app """
-    #     app_resp = self.get_app_links()
-    #
-    #     for app in app_resp:
-    #         if app['label'] == 'AWS_API':
-    #             print(app['linkUrl'])
-    #         if app['label'] == aws_appname:
-    #             return app
-    #
-    #     print("ERROR app not found:", aws_appname)
-    #     sys.exit(2)
-    #
-    # def get_idp_arn(self, app_id):
-    #     """ return the PrincipalArn based on the app instance id """
-    #     headers = self._get_headers()
-    #     response = requests.get(
-    #         self._okta_org_url + '/apps/' + app_id,
-    #         headers=headers,
-    #         verify=True
-    #     )
-    #     app_resp = json.loads(response.text)
-    #     return app_resp['settings']['app']['identityProviderArn']
-    #
-    # def get_role_arn(self, link_url, aws_rolename):
-    #     """ return the role arn for the selected role """
-    #     # decode the saml so we can find our arns
-    #     # https://aws.amazon.com/blogs/security/how-to-implement-federated-api-and-cli-access-using-saml-2-0-and-ad-fs/
-    #     aws_roles = []
-    #     root = et.fromstring(base64.b64decode(self.get_saml_assertion(link_url)))
-    #
-    #     for saml2attribute in root.iter('{urn:oasis:names:tc:SAML:2.0:assertion}Attribute'):
-    #         if saml2attribute.get('Name') == 'https://aws.amazon.com/SAML/Attributes/Role':
-    #             for saml2attributevalue in saml2attribute.iter(
-    #                     '{urn:oasis:names:tc:SAML:2.0:assertion}AttributeValue'):
-    #                 aws_roles.append(saml2attributevalue.text)
-    #
-    #     # grab the role ARNs that matches the role to assume
-    #     for aws_role in aws_roles:
-    #         chunks = aws_role.split(',')
-    #         if aws_rolename in chunks[1]:
-    #             return chunks[1]
-    #
-    #     # if you got this far something went wrong
-    #     print("ERROR no ARN found for", aws_rolename)
-    #     sys.exit(2)
-    #
-    # def get_saml_assertion(self, app_url):
-    #     """return the base64 SAML value object from the SAML Response"""
-    #     if self._saml_assertion is None:
-    #         response = requests.get(
-    #             app_url + '/?sessionToken=' + self._session_token,
-    #             verify=True
-    #         )
-    #
-    #         saml_soup = BeautifulSoup(response.text, "html.parser")
-    #         for inputtag in saml_soup.find_all('input'):
-    #             if inputtag.get('name') == 'SAMLResponse':
-    #                 self._saml_assertion = inputtag.get('value')
-    #
-    #     return self._saml_assertion
+        return self.aws_access[int(selection)]
 
+    def get_app_by_name(self, appname):
+        """ returns the app with the matching name"""
+        for i, app in enumerate(self.aws_access):
+            if app["name"] == appname:
+                return app
 
+    def get_role_by_name(self, app_info, rolename):
+        """ returns the role with the matching name"""
+        for i, role in enumerate(app_info['roles']):
+            if role["name"] == rolename:
+                return role
+
+    def choose_role(self, app_info):
+        """ gets a list of available roles and
+        asks the user to select the role they want to assume
+        """
+
+        print("Pick a role:")
+        # print out the roles and let the user select
+        for i, role in enumerate(app_info['roles']):
+            print('[', i, ']', role["name"])
+
+        selection = input("Selection: ")
+
+        # make sure the choice is valid
+        if int(selection) > len(app_info['roles']):
+            print("You selected an invalid selection")
+            sys.exit(1)
+
+        return app_info['roles'][int(selection)]
+
+    def get_saml_assertion(self, app_url):
+        """return the base64 SAML value object from the SAML Response"""
+        response = self.req_session.get(
+            app_url,
+            verify=self._verify_ssl_certs
+        )
+
+        # parse the SAML response from the HTML
+        saml_soup = BeautifulSoup(response.text, "html.parser")
+        for inputtag in saml_soup.find_all('input'):
+            if inputtag.get('name') == 'SAMLResponse':
+                self._saml_assertion = inputtag.get('value')
+
+        return self._saml_assertion
 
     def _get_username_password_creds(self):
         """Get's creds for Okta login from the user."""
