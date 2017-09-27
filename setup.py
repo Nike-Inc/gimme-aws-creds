2433a46036ae269525e0d30e66358eef116b54b2
Pierce, Eric <Eric.Pierce@nike.com>
2017-09-27 14:50:11 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='gimme aws creds',
-    version='0.1.6',
+    version='0.1.7',
     install_requires=requirements,
     author='Ann Wallace',
     author_email='ann.wallace@nike.com',
