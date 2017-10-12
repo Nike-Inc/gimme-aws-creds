97b60d4ef3e220d4ec2cccdad767142ab8361e57
Justin Wiley <justin.wiley@nike.com>
2017-10-12 14:21:29 -0700

    Increment version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='gimme aws creds',
-    version='1.0.0',
+    version='1.0.1',
     install_requires=requirements,
     author='Ann Wallace',
     author_email='ann.wallace@nike.com',
