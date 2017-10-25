2bccdb35a9e2ff9b2526aba1e550bb792e584121
Pierce, Eric <Eric.Pierce@nike.com>
2017-10-25 14:09:18 -0700

    Add tests for _get_role and _get_app

--- tests/test_main.py
+++ tests/test_main.py
@@ -51,3 +51,47 @@ class TestMain(unittest.TestCase):
         creds = GimmeAWSCreds()
         self.assertRaises(SystemExit, creds._choose_role, self.APP_INFO)
         self.assertRaises(SystemExit, creds._choose_app, self.AWS_INFO)
+
+    def test_get_app_from_config_0(self):
+        creds = GimmeAWSCreds()
+
+        selection = creds._get_app('test1', self.AWS_INFO)
+        self.assertEqual(selection, self.AWS_INFO[0])
+
+    def test_get_app_from_config_1(self):
+        creds = GimmeAWSCreds()
+
+        selection = creds._get_app('test2', self.AWS_INFO)
+        self.assertEqual(selection, self.AWS_INFO[1])
+
+    @patch('builtins.input', return_value='0')
+    def test_missing_app_from_config(self, mock):
+        creds = GimmeAWSCreds()
+
+        selection = creds._get_app('test3', self.AWS_INFO)
+        self.assertEqual(selection, self.AWS_INFO[0])
+
+    def test_get_role_from_config_0(self):
+        creds = GimmeAWSCreds()
+
+        selection = creds._get_role('test1', self.APP_INFO)
+        self.assertEqual(selection, 'test1')
+
+    def test_get_role_from_config_1(self):
+        creds = GimmeAWSCreds()
+
+        selection = creds._get_role('test2', self.APP_INFO)
+        self.assertEqual(selection, 'test2')
+
+    def test_get_role_all(self):
+        creds = GimmeAWSCreds()
+
+        selection = creds._get_role('all', self.APP_INFO)
+        self.assertEqual(selection, 'all')
+
+    @patch('builtins.input', return_value='0')
+    def test_missing_role_from_config(self, mock):
+        creds = GimmeAWSCreds()
+
+        selection = creds._get_role('test3', self.APP_INFO)
+        self.assertEqual(selection, 'test1')
