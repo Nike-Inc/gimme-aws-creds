720733522b39936116b6dc5296b8dafc8ffe64be
Justin Wiley <justin.wiley@nike.com>
2017-11-10 12:49:24 -0800

    Added a .py script for Windows compatibility.

---
+++ bin/gimmie-aws-creds.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Copyright 2016-present Nike, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and* limitations under the License.*
+"""
+from gimme_aws_creds.main import GimmeAWSCreds
+
+if __name__ == '__main__':
+    try:
+        GimmeAWSCreds().run()
+    except KeyboardInterrupt:
+        pass
