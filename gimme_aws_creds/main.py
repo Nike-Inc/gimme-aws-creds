b9e613a3e3b3c1ddcf8ceceb7bbeaa75a4dda51c
ADM-Steiner Pierre <ADM-aaaa@mediait.ch>
2019-08-23 17:10:36 +0200

    ...

--- gimme_aws_creds/main.py
+++ gimme_aws_creds/main.py
@@ -498,10 +498,10 @@ class GimmeAWSCreds(object):
                 #Print out temporary AWS credentials.  Credentials are printed to stderr to simplify
                 #redirection for use in automated scripts
                 if(platform.system()=='Windows'):
-                    print("$env:AWS_ACCESS_KEY_ID='" + aws_creds['AccessKeyId']+"'")
-                    print("$env:AWS_SECRET_ACCESS_KEY='" + aws_creds['SecretAccessKey']+"'")
-                    print("$env:AWS_SESSION_TOKEN='" + aws_creds['SessionToken']+"'")
-                    print("$env:AWS_SECURITY_TOKEN='" + aws_creds['SessionToken']+"'")
+                    print("$env:AWS_ACCESS_KEY_ID='" + aws_creds['AccessKeyId'] + "'")
+                    print("$env:AWS_SECRET_ACCESS_KEY='" + aws_creds['SecretAccessKey'] + "'")
+                    print("$env:AWS_SESSION_TOKEN='" + aws_creds['SessionToken'] + "'")
+                    print("$env:AWS_SECURITY_TOKEN='" + aws_creds['SessionToken'] + "'")
                 else:
                     print("export AWS_ACCESS_KEY_ID=" + aws_creds['AccessKeyId'])
                     print("export AWS_SECRET_ACCESS_KEY=" + aws_creds['SecretAccessKey'])
