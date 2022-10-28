6fc7b67db6b4059339398b8846011d928c5d75d9
Zeph / Liz Loss-Cutler-Hull <warp@aehallh.com>
2022-10-28 09:36:07 -0700

    Correct the check for disable_session.
    Little things...

--- gimme_aws_creds/main.py
+++ gimme_aws_creds/main.py
@@ -562,7 +562,7 @@ class GimmeAWSCreds(object):
         else:
             okta.set_use_keyring(True)
 
-        if self.conf_dict.get('disable_session', 'False') not in ('n', 'false', 'False'):
+        if self.conf_dict.get('disable_session', 'False') not in ('y', 'true', 'True'):
             if self.conf_dict.get('session_token') is not None and self.conf_dict.get('session_username') is not None:
                 okta.set_session_token(self.conf_dict.get('session_username'), self.conf_dict.get('session_token'))
 
