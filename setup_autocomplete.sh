40ba3af7b9b90ec55cf022806876348cdac744ef
Creyf, Jo <jo.creyf@gmail.com>
2022-12-06 23:43:57 -0800

    Adding CLI autocomplete installer

--- setup_autocomplete.sh
+++ setup_autocomplete.sh
@@ -9,6 +9,7 @@ if [ "$(basename $SHELL)" = "bash" ] && [ -f ~/.bashrc ] ; then
   grep -q 'gimme-aws-creds-autocomplete' ~/.bashrc
   if [[ $? -ne 0 ]] ; then
     echo "Adding autocomplete setup to ~/.bashrc"
+    echo "Source in .bashrc or simply restart your shell."
     echo "" >> ~/.bashrc
     echo "# Loading gimme-aws-creds CLI autocomplete:" >> ~/.bashrc
     echo "source ${INSTALL_DIR}/gimme-aws-creds-autocomplete.sh" >> ~/.bashrc
@@ -17,10 +18,13 @@ elif [ "$(basename $SHELL)" = "zsh" ] && [ -f ~/.zshrc ] ; then
   grep -q 'gimme-aws-creds-autocompletion' ~/.zshrc
   if [[ $? -ne 0 ]] ; then
     echo "Adding autocomplete setup to ~/.zshrc"
+    echo "Source in .zshrc or simply restart your shell."
     echo "" >> ~/.zshrc
     echo "# Loading gimme-aws-creds CLI autocomplete:" >> ~/.zshrc
     echo "autoload bashcompinit" >> ~/.zshrc
     echo "bashcompinit" >> ~/.zshrc
     echo "source ${INSTALL_DIR}/gimme-aws-creds-autocomplete.sh" >> ~/.zshrc
   fi
+else
+  echo "Not sure how to install this in your shell: $(basename $SHELL)"
 fi
