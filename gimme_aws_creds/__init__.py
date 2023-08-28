b5e87bed64dfc8d4923fadb4f155d2adaa749c23
Eric Pierce <eric@pierce.cc>
2023-08-28 10:24:59 -0700

    Bump version number

--- gimme_aws_creds/__init__.py
+++ gimme_aws_creds/__init__.py
@@ -1,2 +1,2 @@
 __all__ = ['config', 'aws', 'main', 'ui', 'common', 'default', 'duo', 'errors', 'okta_classic', 'okta_identity_engine', 'registered_authenticators', 'u2f', 'webauthn']
-version = '2.7.1'
+version = '2.7.2'
