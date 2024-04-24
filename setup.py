665321325f2db61b9abff1aff6ff6587a5c3e5fe
Eric Pierce <Eric.Pierce@nike.com>
2024-04-24 11:31:42 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='gimme_aws_creds',
-    version='2.8.1',
+    version='2.8.1.1',
     install_requires=requirements,
     author='Eric Pierce',
     author_email='eric.pierce@nike.com',
